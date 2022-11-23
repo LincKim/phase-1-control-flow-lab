ddc97a29039ad94e1f671078629e26b341a5fb87
LincKim <lincolnkimutai@gmail.com>
2022-11-23 16:46:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(dist){
   // Write your code here!
+  if (dist <= '400') {
+      return 'This one is on me!'
+  } else if(dist > '2000' && dist < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (dist > '2500') {
+    return 'No can do.'
+  }
+  
+}
+scuberGreetingForFeet(199)
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === 'NYC'?'Ok, sounds good.' :'No go.'
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+      
+      break;
+  
+    default:
+      return 'Bye.'
+      break;
+  }
 }
